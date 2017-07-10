f4dfcc3ebb1bb96a7110bd2ce02932a101886feb
Epowerj <epowerj@gmail.com>
2017-07-10 01:13:44 -0400

    Added more debug

--- noteparser.py
+++ noteparser.py
@@ -4,6 +4,8 @@ from html.parser import HTMLParser
 class NoteParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         print ("Encountered a start tag:", tag)
+        if (tag "a"):
+            print(attrs)
 
     def handle_endtag(self, tag):
         print ("Encountered an end tag :", tag)
