d74338da3c80a125c24c6e184d306314b1951993
Epowerj <epowerj@gmail.com>
2017-07-10 00:10:55 -0400

    Added basic code

---
+++ bot.py
@@ -0,0 +1,76 @@
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+from key import apikey
+from urllib.parse import urlparse
+import os, logging, datetime, time
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+def start(bot, update):
+    bot.sendMessage(update.message.chat_id, text="Hey!")
+
+
+def help(bot, update):
+    bot.sendMessage(update.message.chat_id, text='Just ask @Epowerj. This bot currently uses a dev build.')
+
+def ping(bot, update):
+    bot.sendMessage(update.message.chat_id, text='Pong')
+
+
+def time(bot, update):
+    bot.sendMessage(update.message.chat_id, text=str(datetime.datetime.now()))
+
+
+def chatinfo(bot, update):
+    bot.sendMessage(update.message.chat_id, text="chat_id is "+str(update.message.chat_id))
+    bot.sendMessage(update.message.chat_id, text="user id is "+str(update.message.from_user.id))
+
+
+def error(bot, update, error):
+    print('Update "%s" caused error "%s"' % (update, error))
+
+
+def parse(bot, update):
+    #print(str(update.channel_post.chat_id))
+    print("file: " + str(update.message.document.file_id))
+    print("Message from " + update.message.from_user.first_name + "(" + str(update.message.from_user.id) + "): " +
+          update.message.text + " (" + str(update.message.message_id) + ")")
+
+
+def main():
+    global char_info
+    global next_action
+
+    load_info()
+    print(char_info)
+    print(next_action)
+
+    TOKEN = apikey
+    PORT = int(os.environ.get('PORT', '5000'))
+    updater = Updater(TOKEN)
+    dp = updater.dispatcher
+    # add handlers
+    updater.start_webhook(listen="0.0.0.0",
+                      port=PORT,
+                      url_path=TOKEN)
+    updater.bot.setWebhook("https://" + str(os.environ.get("APPNAME")) + ".herokuapp.com/" + TOKEN)
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("ping", ping))
+    dp.add_handler(CommandHandler("time", time))
+    dp.add_handler(CommandHandler("chatinfo", chatinfo))
+
+    dp.add_handler(MessageHandler([Filters.text], parse))
+
+    dp.add_error_handler(error)
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
